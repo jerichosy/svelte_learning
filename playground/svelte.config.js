ef69c82c8c54089dd7075c41c7e7a199edd1e018
Matthew Jericho Go Sy <mgsy1101@gmail.com>
2025-08-19 15:58:28 +0800

    WIP: Add asset alias defined by env

--- playground/svelte.config.js
+++ playground/svelte.config.js
@@ -6,7 +6,12 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		alias: {
+			_svelteWelcome: `src/lib/images/${process.env.ORGANIZATION}/svelte-welcome.png`
+		}
+	}
 };
 
 export default config;
